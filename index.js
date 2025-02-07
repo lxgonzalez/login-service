86b75ec57a98abdddd6c1ecd7a100ae35fc2ea3d
keyth05 <keithyaguana@gmail.com>
2025-02-07 11:37:44 -0500

    feat: :sparkles: Modificated route

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const passport = require('passport');
+const cors = require('cors');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const session = require('express-session');
 require('dotenv').config();
